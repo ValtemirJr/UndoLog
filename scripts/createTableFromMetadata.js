3bc501a5bb197300f1ebbd5e74d929c94f133cf8
Valtemir Junior <100030801+ValtemirJr@users.noreply.github.com>
2023-10-22 17:47:16 -0300

    fix: criação de tabela e inserção de dados via json

--- scripts/createTableFromMetadata.js
+++ scripts/createTableFromMetadata.js
@@ -4,29 +4,33 @@ const metadata = require('../dataFiles/metadata.json');
 async function createTableFromMetadata() {
   try {
     const db = await connectToDatabase();
-
     const { table } = metadata;
-
-    // Monta a definição da tabela com base no json
+    
+    // Monta a definição da tabela com base no JSON
     const tableColumns = Object.keys(table).map((columnName) => `${columnName} INT`);
     const createTableSQL = `
       CREATE TABLE IF NOT EXISTS ${table.table_name} (
         ${tableColumns.join(', ')}
       );
     `;
+    
     await db.query(createTableSQL);
     console.log('Tabela criada com sucesso.');
 
-    // Insere os valores iniciais nas colunas
+    // Itera sobre os dados e insere cada tupla
+    const columnNames = Object.keys(table).join(', ');
     const insertInitialValuesSQL = `
-      INSERT INTO ${table.table_name} (${Object.keys(table).join(', ')})
-      VALUES (${Object.values(table).join(', ')})
+      INSERT INTO ${table.table_name} (${columnNames})
+      VALUES 
+      ${table.id.map((id, index) => `(${id}, ${table.A[index]}, ${table.B[index]})`).join(', ')}
     `;
+
     await db.query(insertInitialValuesSQL);
     console.log('Valores iniciais inseridos com sucesso.');
   } catch (error) {
     console.error('Erro ao criar a tabela e inserir os valores iniciais:', error);
   }
 }
+
 
 module.exports = createTableFromMetadata;
