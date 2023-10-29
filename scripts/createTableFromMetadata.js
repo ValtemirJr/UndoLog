b6374718c7d954c42aba82c39d9f72c91b2de2bf
Lucas Smaniotto <lucas91755@gmail.com>
2023-10-29 12:15:15 -0300

    fix: ajustando script de criação de tabela

--- scripts/createTableFromMetadata.js
+++ scripts/createTableFromMetadata.js
@@ -5,6 +5,7 @@ async function createTableFromMetadata() {
   try {
     const db = await connectToDatabase();
     const { table } = metadata;
+    console.log( 'table', table)
 
     // Dropa a tabela se ela já existir
     await db.query(`DROP TABLE IF EXISTS ${table.table_name};`);
@@ -16,16 +17,30 @@ async function createTableFromMetadata() {
         ${tableColumns.join(', ')}
       );
     `;
+
+    console.log('createTableSQL', createTableSQL);
     
     await db.query(createTableSQL);
 
     // Itera sobre os dados e insere cada tupla
     const columnNames = Object.keys(table).join(', ');
+    const numRows = Math.max(...Object.values(table).map(arr => arr.length));
+
+    const insertValues = Array.from({ length: numRows }, (_, index) => {
+      const rowValues = Object.keys(table).map(columnName => {
+        const value = table[columnName][index];
+        return value !== undefined ? value : 'NULL';
+      });
+      return `(${rowValues.join(', ')})`;
+    });
+
     const insertInitialValuesSQL = `
       INSERT INTO ${table.table_name} (${columnNames})
       VALUES 
-        ${table.id.map((id, index) => `(${Object.values(table).map((column) => column[index]).join(', ')})`).join(', ')}
+        ${insertValues.join(', ')}
     `;
+
+    console.log('insertInitialValuesSQL', insertInitialValuesSQL);
 
     await db.query(insertInitialValuesSQL);
 
